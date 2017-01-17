d136dea104d11d68b159583bcbd5d18aee3da7d7
kosiorski-eu <konrad@kosiorski.eu>
2017-01-17 14:58:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-(function($) {
+function Person(firstName, lastName) {
+ 
+    if( !(this instanceof Person) ) {
+        return new Person(firstName, lastName);
+    }
+ 
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.attempts = 0;
+ 
+}
+ 
+Person.prototype.flirt = function() {
 
-    $(document).ready(function() {
-		
-		//wszystkie elementy <div>, posiadające zarówno klasę “grid” oraz klasę “grid-12”
-		var div = $("div.grid, div.grid-12");
-		
-		//wszystkie elementy <a>, których atrybut “href" zaczyna się na “http”, znajdujące się w elemencie o klasie “nav”
-		var a = $(".nav a[href^='http']");
-		
-		//wszystkie elementy <input>, których typ to “radio” lub “checkbox” oraz dodatkowo nie są aktualnie zaznaczone (checked)
-		var input = $("input[type='radio'], input[type='checkbox']").not(":checked");
-		
-		//wyłącznie pierwszy element <p>, który jest pusty (nie zawiera dzieci) oraz znajduje się w elemencie <div> z identyfikatorem “text“
-		var p = $("div#text p:first:empty");
-		
-		//wszystkie elementy z klasą “pagination-item”, które nie są elementem <span>
-		var i = $(":not(span).pagination-item");
-
-    });
-
-})(jQuery);
-
-
-
-
-
+    this.interval = setInterval(()=>{
+        console.log("Nazywam się " + this.firstName + " " + this.lastName + "!");
+            if(++this.attempts === 10) {
+            clearInterval(this.interval);
+        }
+    },1000);
+ 
+};
+ 
+var person = new Person("Jan", "Nowak");
+ 
+person.flirt();
 
 
 
